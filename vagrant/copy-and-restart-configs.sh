65b979a8c649a0c2d9185f97eec296c849eb34c4
Nelson LaQuet <nelsonlaquet@gmail.com>
2015-04-30 02:23:14 -0500

    Files can now upload. This requires you to reprovision your VM.

--- vagrant/copy-and-restart-configs.sh
+++ vagrant/copy-and-restart-configs.sh
@@ -1,6 +1,8 @@
 sudo cp /vagrant/vagrant/pony.fm.nginx.config /etc/nginx/nginx.conf
 sudo cp /vagrant/vagrant/pony.fm.nginx.site.config /etc/nginx/sites-enabled/pony.fm
 
+sudo cp /vagrant/vagrant/php.ini /etc/php5/fpm/php.ini
+
 sudo cp /vagrant/vagrant/pony.fm.mysql.config /etc/mysql/my.cnf
 
 sudo cp /vagrant/vagrant/pony.fm.redis.config /etc/redis/redis.conf
--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,3 +1,9 @@
+sudo add-apt-repository -y ppa:kirillshkrogalev/ffmpeg-next
+sudo apt-get update
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y AtomicParsley
+sudo add-apt-repository -y --remove ppa:kirillshkrogalev/ffmpeg-next
+
 mkdir /vagrant/logs
 /vagrant/vagrant/copy-and-restart-configs.sh
 
