33753d84114c985de80ce6099f9e141181cbae37
Kelvin Zhang <me@iamkelv.in>
2015-09-08 11:57:28 +0100

    Update Kernel.php for commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\Inspire::class,
+        \App\Console\Commands\MigrateOldData::class,
+        \App\Console\Commands\RefreshCache::class,
     ];
 
     /**
@@ -24,7 +25,5 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
     }
 }
