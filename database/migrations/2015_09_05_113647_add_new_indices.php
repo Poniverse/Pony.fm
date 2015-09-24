7d76e2d74dc93db4c1682566fee120117a712f2f
Peter Deltchev <peter@deltchev.com>
2015-09-24 09:55:34 -0700

    Fixed a broken migration and cleaned up some leftover files from Laravel 4..

--- database/migrations/2015_09_05_113647_add_new_indices.php
+++ database/migrations/2015_09_05_113647_add_new_indices.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNewIndicies extends Migration
+class AddNewIndices extends Migration
 {
     /**
      * Run the migrations.
