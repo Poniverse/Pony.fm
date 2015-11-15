ac533a6eeb394a70f79f5a773f5e73b862d72e95
Peter Deltchev <peter@deltchev.com>
2015-11-15 00:52:48 -0800

    Simplified the transaction names we send to NewRelic.

--- config/newrelic.php
+++ config/newrelic.php
@@ -31,7 +31,7 @@ return array(
      *          "GET /world" with pattern 'hello {path} you really {method} me' would return:
      *          'hello /world you really GET me'
      */
-    'name_provider' => '{uri} {route}',
+    'name_provider' => '{route}',
 
     /*
      * Will cause an exception to be thrown if the NewRelic
