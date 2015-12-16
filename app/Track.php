cac58df6f4803f0642bbbda3e9ad54f17a3bf8c7
Zeusking19 <zeusking19@poniverse.net>
2015-12-16 18:52:40 +0000

    Whoops - forgot something ;)

--- app/Track.php
+++ app/Track.php
@@ -308,7 +308,7 @@ class Track extends Model
             'is_explicit' => (bool)$track->is_explicit,
             'is_downloadable' => (bool)$track->is_downloadable,
             'is_published' => (bool)$track->isPublished(),
-            'published_at' => $track->published_at,
+            'published_at' => $track->published_at->format('c'),
             'duration' => $track->duration,
             'genre' => $track->genre != null
                 ?
