a8e81729fbeef310648d1a9fce79b38af0467a23
Peter Deltchev <peter@deltchev.com>
2016-07-09 17:59:03 -0700

    Run asynchronous workers in the dev environment.
    This better matches how Pony.fm is deployed in production and avoids issues
    with the database being reconnected in the middle of a database transaction.

--- vagrant/copy-and-restart-configs.sh
+++ vagrant/copy-and-restart-configs.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Updating config files and restarting services..."
+echo
+
 mkdir -p /vagrant/storage/app/datastore
 
-sudo cp /vagrant/vagrant/pony.fm.nginx.config /etc/nginx/nginx.conf
-sudo cp /vagrant/vagrant/pony.fm.nginx.site.config /etc/nginx/sites-enabled/pony.fm
+sudo cp /vagrant/vagrant/pony.fm.nginx.config /etc/nginx/nginx.conf &
+sudo cp /vagrant/vagrant/pony.fm.nginx.site.config /etc/nginx/sites-enabled/pony.fm &
+sudo cp /vagrant/vagrant/php-overrides.ini /etc/php/7.0/fpm/99-overrides.ini &
+sudo cp /vagrant/vagrant/pony.fm.redis.config /etc/redis/redis.conf &
+sudo cp /vagrant/vagrant/pony.fm.supervisor.config /etc/supervisor/conf.d/pony.fm.conf &
+wait
 
-sudo cp /vagrant/vagrant/php-overrides.ini /etc/php/7.0/fpm/99-overrides.ini
-
-sudo cp /vagrant/vagrant/pony.fm.redis.config /etc/redis/redis.conf
-
-sudo service elasticsearch restart
-sudo service nginx restart
-sudo service php7.0-fpm restart
+sudo supervisorctl update &
+sudo service elasticsearch restart &
+sudo service nginx restart &
+sudo service php7.0-fpm restart &
+wait
