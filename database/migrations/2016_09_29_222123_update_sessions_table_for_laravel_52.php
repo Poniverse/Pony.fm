97bf384e1b8a910f1841cc5e4a9c72471901d1e5
Adam Lavin <adam@lavoaster.co.uk>
2016-09-29 23:24:36 +0100

    Added laravel 5.2 sessions migration

---
+++ database/migrations/2016_09_29_222123_update_sessions_table_for_laravel_52.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSessionsTableForLaravel52 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sessions', function(Blueprint $table) {
+            $table->integer('user_id')->nullable();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sessions', function(Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('ip_address');
+            $table->dropColumn('user_agent');
+        });
+    }
+}
