f8d2fc1580f71c3628ea948c92f1c159fd45e002
Laravel Shift <shift@laravelshift.com>
2016-09-29 21:27:19 +0000

    Shift core files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,11 +21,12 @@
 namespace Poniverse\Ponyfm\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests, AuthorizesRequests;
 }
