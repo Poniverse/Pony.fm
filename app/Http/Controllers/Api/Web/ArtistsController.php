1c40d3cfa552c9308ee7cdc0c645b2ae71bcbfb4
Josef Citrine <josefcitrine@googlemail.com>
2016-11-12 21:13:29 +0000

    Fixed server crashing if avatar can't be found

--- app/Http/Controllers/Api/Web/ArtistsController.php
+++ app/Http/Controllers/Api/Web/ArtistsController.php
@@ -172,8 +172,18 @@ class ArtistsController extends ApiControllerBase
             ];
         }
 
-        $palette = ColorThief::getPalette($user->getAvatarUrl(Image::SMALL), 2);
-        $formatted_palette = array_map("Helpers::rgb2hex", $palette);
+        $formatted_palette = [
+            "#ffffff",
+            "#ffffff",
+            "#ffffff"
+        ];
+
+        try {
+            $palette = ColorThief::getPalette($user->getAvatarUrl(Image::SMALL), 2);
+            $formatted_palette = array_map("Helpers::rgb2hex", $palette);
+        } catch (Exception $e) {
+            // We failed to get the image, oh well
+        }
 
         $followers = Follower::where('artist_id', $user->id)
             ->count();
