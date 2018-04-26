96f1317943cfa29a73917fccf1a5a64f4ba2068d
Adam Lavin <adam@adamlav.in>
2018-04-26 13:07:29 +0100

    Fix config for register redirections

--- config/poniverse.php
+++ config/poniverse.php
@@ -1,5 +1,8 @@
 <?php
 return [
     'client_id' => env('PONI_CLIENT_ID'),
-    'secret' => env('PONI_CLIENT_SECRET')
+    'secret' => env('PONI_CLIENT_SECRET'),
+    'urls' => [
+        'register' => 'https://poniverse.net/register',
+    ],
 ];
