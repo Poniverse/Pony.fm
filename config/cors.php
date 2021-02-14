7b64f5b1a7fa6c11972b4763887d77c6d4720dc3
Adam Lavin <adam@lavoaster.co.uk>
2021-02-14 19:09:50 +0000

    Updates after 7.0 shift

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['localhost:3000', 'pony.fm', 'stage.pony.fm'],
 
     'allowed_origins_patterns' => [],
 
