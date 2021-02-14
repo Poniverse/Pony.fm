a6f58f867fe15d3cd413b34476910caa258c32bc
Laravel Shift <shift@laravelshift.com>
2021-02-14 18:57:23 +0000

    Shift core files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -65,7 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
         'auth.oauth' => \App\Http\Middleware\AuthenticateOAuth::class,
         'json-exceptions' => \App\Http\Middleware\JsonExceptions::class,
     ];
